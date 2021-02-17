8fe1a0a649cca25555df9ed2d1c6e4370f1d7323
Oscar Dahlqvist <oscdah@student.chalmers.se>
2021-02-17 14:53:34 +0100

    1 a,b klar?

--- assignment 2/ass2.hs
+++ assignment 2/ass2.hs
@@ -1,41 +1,58 @@
-{-# LANGUAGE GADTs, FlexibleContexts, FlexibleInstances, TypeSynonymInstances #-}
+{-# LANGUAGE GADTs, FlexibleContexts, FlexibleInstances, TypeSynonymInstances, StandaloneDeriving #-}
 import Data.List
 import Test.QuickCheck
 
---1a
+-- ============== 1a ============== --
 
-import qualified Prelude (sin,cos,exp)
-import Prelude hiding (recip,negate,exp,sin,cos)
+import qualified Prelude (sin,cos,exp,pi)
+import Prelude hiding (recip,negate,sin,cos,exp,pi)
 
 type REAL = Double
 
-eval :: FunExp -> FunSem
-eval expr x = error "todo"
+eval :: Transcendental a => FunExp a -> (a -> a)
+eval (Zero)     = \x -> zero
+eval (One)      = \x -> one
+eval (Con c)    = \x -> c
+eval (FunX)     = \x -> x 
+eval (Add a b)  = \x -> eval a x `add` eval b x
+eval (Mul a b)  = \x -> eval a x `mul` eval b x
+eval (Negate a) = \x -> negate (eval a x)
+eval (Recip a)  = \x -> recip (eval a x)
+eval (Root a)   = \x -> root (eval a x)
+eval (Pi)       = \x -> pi
+eval (Sin a)    = \x -> sin (eval a x)
+eval (Cos a)    = \x -> cos (eval a x)
+eval (Exp a)    = \x -> exp (eval a x)
 
-eval' :: FunExp -> FunSem
-eval'= eval.derive
 
-eval'' :: FunExp -> FunSem
-eval'' = eval'.derive
+--eval' :: Transcendental a => FunExp -> FunSem
+--eval'= eval.derive
+--
+--eval' :: Transcendental a => FunExp -> FunSem
+--eval'' = eval'.derive
 --eval'' = eval.derive.derive
 
-derive :: FunExp -> FunExp
+derive :: FunExp a -> FunExp a
 derive = error "todo"
 
-data FunExp where
- Con    :: REAL -> FunExp
- FunX   :: FunExp
- Add    :: FunExp -> FunExp -> FunExp
- Mul    :: FunExp -> FunExp -> FunExp
- Negate :: FunExp -> FunExp
- Recip  :: FunExp -> FunExp
- Root   :: FunExp -> FunExp
- Pi     :: FunExp
- Sin    :: FunExp -> FunExp
- Cos    :: FunExp -> FunExp
- Exp    :: FunExp -> FunExp
-
-type FunSem = (REAL -> REAL)
+data FunExp a where
+ Zero   :: FunExp a --bad
+ One    :: FunExp a --bad
+ Con    :: (Transcendental a) => a -> FunExp a
+ FunX   :: FunExp a
+ Add    :: FunExp a -> FunExp a -> FunExp a
+ Mul    :: FunExp a -> FunExp a -> FunExp a
+ Negate :: FunExp a -> FunExp a
+ Recip  :: FunExp a -> FunExp a
+ Root   :: FunExp a -> FunExp a
+ Pi     :: FunExp a
+ Sin    :: FunExp a -> FunExp a
+ Cos    :: FunExp a -> FunExp a
+ Exp    :: FunExp a -> FunExp a
+ 
+deriving instance Show (FunExp REAL)
+ 
+--type FunSem = (REAL -> REAL)
 
 {-
 By def. the homomorphism conserves the operation, in this case eval''. Thus if eval'' is a homomorphism,
@@ -73,6 +90,8 @@ eval'' $ (FunX) `Mul` (FunX `Add` (Con 1)) <==> omul (\r -> 0) (\r -> 0)
 which is false, hence statement H2 is false.
 -}
 
+-- ============== 1b ============== --
+
 type Tri a    = (a,a,a)
 type TriFun a = Tri (a -> a)    -- (a->a, a->a, a->a)
 type FunTri a = a -> Tri a      -- a -> (a,a,a)
@@ -95,27 +114,27 @@ class (MulGroup a, AddGroup a) => Algebraic a where
  root   :: a -> a
  
 class Algebraic a => Transcendental a where
- π      :: a
+ pi     :: a
  sin    :: a -> a
  cos    :: a -> a
  exp    :: a -> a
  
 --dd :: Transcendental a => FunExp -> (Tri FunExp)
 
-instance Additive FunExp where
- zero   = Con 0
+instance Additive a => Additive (FunExp a) where
+ zero   = Zero
  add    = Add
-instance Multiplicative FunExp where
- one    = Con 1
+instance Multiplicative a => Multiplicative (FunExp a) where
+ one    = One
  mul    = Mul
-instance AddGroup FunExp where
+instance AddGroup a => AddGroup (FunExp a) where
  negate = Negate
-instance MulGroup FunExp where
+instance MulGroup a => MulGroup (FunExp a) where
  recip  = Recip
-instance Algebraic FunExp where
+instance Algebraic a => Algebraic (FunExp a) where
  root   = Root
-instance Transcendental FunExp where
- π      = Pi
+instance Transcendental a => Transcendental (FunExp a) where
+ pi     = Pi
  sin    = Sin
  cos    = Cos
  exp    = Exp
@@ -133,45 +152,42 @@ instance MulGroup REAL where
 instance Algebraic REAL where
  root   = sqrt
 instance Transcendental REAL where
- π      = pi
+ pi     = Prelude.pi
  sin    = Prelude.sin
  cos    = Prelude.cos
  exp    = Prelude.exp
 
-d :: FunExp -> FunExp
+d :: Transcendental a => FunExp a -> FunExp a
 d (Con _)       = zero
 d (FunX)        = one
 d (Add a b)     = add (d a) (d b)
 d (Mul a b)     = add (a `mul` d b) (d a `mul` b)
 d (Negate a)    = negate (d a)
-d (Recip a)     = (negate $ recip (a `mul` a)) `mul` (d a)      --1/f(x) = -¹/f(x)² * f'(x)
+d (Recip a)     = (negate $ recip (a `mul` a)) `mul` (d a)                --1/f(x) = -¹/f(x)² * f'(x)
 d (Root a)      = (recip $ (root a `mul` (one `add` one))) `mul` (d a)    --f(x)¹ᐟ² = ⁻¹/(2f(x)) * f'(x)
 d (Pi)          = zero
 d (Sin a)       = (cos a) `mul` (d a)
 d (Cos a)       = (negate $ sin a) `mul` (d a)
 d (Exp a)       = (exp a) `mul` (d a)
+d _             = zero
 
-dd :: FunExp -> FunExp
+dd :: Transcendental a => FunExp a -> FunExp a
 dd = d.d
 
---evalDD :: Transcendental a => FunExp -> (a -> Tri a)
-evalDD expr = \a -> (f a, f' a, f'' a)
+evalDD :: Transcendental a => FunExp a -> FunTri a
+evalDD expr a = (f a, f' a, f'' a)
  where 
   f   = eval (expr)
   f'  = eval (d expr)
   f'' = eval (dd expr)
 
-{- Con    :: REAL -> FunExp
- FunX   :: FunExp
- Add    :: FunExp -> FunExp -> FunExp
- Mul    :: FunExp -> FunExp -> FunExp
- Negate :: FunExp -> FunExp
- Recip  :: FunExp -> FunExp
- Root   :: FunExp -> FunExp
- Pi     :: FunExp
- Sin    :: FunExp -> FunExp
- Cos    :: FunExp -> FunExp
- Exp    :: FunExp -> FunExp
- -}
+-- ============== 1c ============== --
 
- 
+--step one, surrender
+
+-- ============== 2a ============== --
+
+newton :: (REAL -> REAL) -> REAL -> REAL -> REAL
+newton f ep x = if abs (f x) < ep
+                    then x
+
